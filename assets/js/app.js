83d617f7ba969c104371712190c6fe62e27671cd
HumanJBooF <humanjboof@gmail.com>
2018-08-12 22:19:50 -0400

    typed.js aos.js, added some technoligies and about me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,44 @@
 $(function () {
 
-   
-
-
+    $(".dropdown-trigger").dropdown();
+    $('.tabs').tabs();
+    $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
     $('.sidenav').sidenav({
         draggable: true
     });
     $('.tooltipped').tooltip();
+
+    let options = {
+        strings: ["I am a... <br> Web Developer <br> Check out my work..."],
+        typeSpeed: 70,
+        loop: false,
+        contentType: 'html',
+        showCursor: true,
+        cursorChar: '|',
+        startDelay: 1000,
+    }
+
+    let typed = new Typed(".typed", options);
+  
+    $('.tech').on('click', function () {
+ 
+            
+            let options1 = {
+                strings: ["Languages, Frameworks & Tools"],
+                typeSpeed: 70,
+                loop: false,
+                contentType: 'html',
+                showCursor: true,
+                cursorChar: '|',
+                
+            }
+            let typed = new Typed(".typed1", options1);
+        
+    });
+    AOS.init({
+        duration: 1500,
+        mirror: false,
+    });
+
 })
