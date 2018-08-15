f51a8dc55a058a03a7bf27292d17e5875cce5272
HumanJBooF <humanjboof@gmail.com>
2018-08-14 21:53:30 -0400

    added some bold to text

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 $(function () {
 
-    $('.collapsible').collapsible()
-    $('.slider').slider();
+    
     $('.scrollspy').scrollSpy({
         scrollOffset: 100
     });
@@ -9,8 +8,8 @@ $(function () {
     $('.sidenav').sidenav({
         draggable: true
     });
-    $('.tooltipped').tooltip();
-//typed.js objects
+
+    //typed.js objects
     let options = {
         strings: ["Welcome to my page! <br> I am a Web Developer <br>Come check out my work..."],
         typeSpeed: 60,
@@ -22,20 +21,12 @@ $(function () {
         backSpeed: 20,
         backDelay: 5000
     }
-    
-    let options1 = {
-        strings: ["Languages, Frameworks & Tools"],
-        typeSpeed: 60,
-        loop: true,
-        contentType: 'html',
-        showCursor: true,
-        cursorChar: '|',
-        startDelay: 2000,
-    }
-//typed.js grab each element
-    let typed = new Typed(".typed", options);
-    let typed1 = new Typed(".typed1", options1)
+
    
+    //typed.js grab each element
+    let typed = new Typed(".typed", options);
+    
+
     AOS.init({
         duration: 1500,
         mirror: false,
